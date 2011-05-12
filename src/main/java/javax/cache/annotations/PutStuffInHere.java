dc6e98e124eb8bbf24bc83489b09709faa24017c
Greg Luck <gluck@gregluck.com>
2011-05-12 13:32:38 -0700

    Create annotations here

---
+++ src/main/java/javax/cache/annotations/PutStuffInHere.java
@@ -0,0 +1,11 @@
+package javax.cache.annotations;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: gluck
+ * Date: 12/05/11
+ * Time: 1:32 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class PutStuffInHere {
+}
