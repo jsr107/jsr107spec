6b1fcdf9192a5a41fe0f2b788122961f461ef967
Greg Luck <gluck@gregluck.com>
2011-05-19 14:44:46 +1000

    Add get to CacheManager

--- src/main/java/javax/cache/CacheManager.java
+++ src/main/java/javax/cache/CacheManager.java
@@ -2,6 +2,7 @@ package javax.cache;
 
 /**
  * A CacheManager is used for looking up Caches and controls their lifecycle.
+ *
  * @author Greg Luck
  */
 public interface CacheManager {
@@ -10,17 +11,30 @@ public interface CacheManager {
     /**
      * This method will return a TransactionManager which can only be used for
      * local transactions, not XA Transactions.
+     *
      * @return the UserTransaction
      */
     javax.transaction.UserTransaction getUserTransaction();
 
 
     /**
+     * Adds a {@link Cache} to the CacheManager.
+     * <p/>
      *
-     * @param cacheName
-     * @return
+     *
+     * @param cache
+     * @throws IllegalStateException if the cache is not {@link Status#STATUS_UNINITIALISED} before this method is called.
+     * @throws CacheException        if there was an error adding the cache to the CacheManager
      */
-    Cache getCache(String cacheName);
+    void addCache(Cache<?, ?> cache) throws IllegalStateException, CacheException;
+
+
+    /**
+     * Gets a named cache.
+     * @param cacheName
+     * @return the Cahce or null if it does exist
+     */
+    <K, V> Cache<K, V> getCache(String cacheName);
 
 
 }
