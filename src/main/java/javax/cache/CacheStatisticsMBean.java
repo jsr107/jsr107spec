de294cb348998e7de9d4a61880d5bcbef019b3d0
Greg Luck <gluck@gregluck.com>
2011-07-15 15:49:43 +1000

    Finish complete pass of CacheStatisticsMBean

--- src/main/java/javax/cache/CacheStatisticsMBean.java
+++ src/main/java/javax/cache/CacheStatisticsMBean.java
@@ -8,33 +8,68 @@
 package javax.cache;
 
 /**
- * todo: finish this
- * Starter for a statistics MBean.
+ * A management bean for caches.
+ *
+ * Statistics are accumulated from the time a cache is created. They can be reset to zero using {@link #clearStatistics()}.
+ *
  * @author Greg Luck
- * @since 1.7
+ * @since 1.0
  */
 public interface CacheStatisticsMBean {
 
     /**
+     * @return the name of the Cache these statistics are for
+     */
+    String getName();
+
+
+    /**
+     * Gets the {@link Status} attribute of the Cache expressed as a String.
      *
+     * @return The status value from the Status enum class
+     */
+    String getStatus();
+
+
+    /**
+     * Clears the statistics counters to 0 for the associated Cache.
+     */
+    void clearStatistics();
+
+
+    /**
      * @return the number of entries
      */
     long getEntryCount();
 
     /**
-     *
-     * @return the number if hits
+     * @return the number of hits
      */
     long getCacheHits();
 
     /**
+     * Returns the percentage of cache accesses that found a requested item in the cache.
      *
+     * @return the percentage of successful hits, as a decimal
+     */
+    double getCacheHitPercentage();
+
+    /**
      * @return the number of misses
      */
     long getCacheMisses();
 
     /**
+     * Returns the percentage of cache accesses that did not find a requested entry in the cache.
      *
+     * @return the percentage of accesses that failed to find anything
      */
-    void clearStatistics();
+    double getCacheMissPercentage();
+
+    /**
+     * @return the number of evictions from the cache
+     */
+    long getCacheEvictions();
+
+
 }
