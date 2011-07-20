2bdcb8e53a544549e7c0315c1ef6c8ebee24cde2
Yannis Cosmadopoulos <yannisc@gmail.com>
2011-07-20 11:59:02 -0700

    Proposed CacheLoader change

--- src/main/java/javax/cache/CacheLoader.java
+++ src/main/java/javax/cache/CacheLoader.java
@@ -36,6 +36,18 @@ public interface CacheLoader<K, V> {
     V load(K key, Object arg);
 
     /**
+     * loads an object. Application writers should implement this
+     * method to customize the loading of cache object. This method is called
+     * by the caching service when the requested object is not in the cache.
+     * <p/>
+     *
+     * @param key the key identifying the object being loaded
+     * @param arg the argument passed
+     * @return The entry for the object that is to be stored in the cache.
+     */
+    Cache.Entry<K, V> loadEntry(Object key, Object arg);
+
+    /**
      * loads multiple object. Application writers should implement this
      * method to customize the loading of cache object. This method is called
      * by the caching service when the requested object is not in the cache.
