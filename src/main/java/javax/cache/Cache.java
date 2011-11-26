54849f1e6c236f7049dfaeb0b232b713db6a96d2
Yannis Cosmadopoulos <yannisc@gmail.com>
2011-11-26 07:15:09 -0800

    Tidy up of EntryProcessor - reamed to AtomicOperation, moved to inner class of Cache

--- src/main/java/javax/cache/Cache.java
+++ src/main/java/javax/cache/Cache.java
@@ -482,10 +482,10 @@ public interface Cache<K, V> extends Iterable<Cache.Entry<K, V>>, CacheLifecycle
      *
      *
      * @param key
-     * @param entryProcessor
-     * @return
+     * @param atomicOperation
+     * @return an object
      */
-    Object invokeEntryProcessor(K key, EntryProcessor entryProcessor);
+    Object invokeAtomicOperation(K key, AtomicOperation atomicOperation);
 
     /**
      * Return the name of the cache.
@@ -537,5 +537,70 @@ public interface Cache<K, V> extends Iterable<Cache.Entry<K, V>>, CacheLifecycle
          * @return the value corresponding to this entry
          */
         V getValue();
+    }
+
+    /**
+     * An accessor and mutator to the underlying Cache
+     * @param <K>
+     * @param <V>
+     */
+    public interface MutableEntry<K, V> extends Entry<K, V> {
+
+        /**
+         * Checks for the existence of the entry in the cache
+         * @return
+         */
+        boolean exists();
+
+        /**
+         * Removes the entry from the Cache
+         */
+        void remove();
+
+        /**
+         * Sets or replaces the value associated with the key
+         * If {@link #exists} is false and setValue is called
+         * then a mapping is added to the cache visible once the AtomicOperation
+         * completes. Moreover a second invocation of {@link #exists()}
+         * will return true.
+         * @param value
+         */
+        void setValue(V value);
+    }
+
+    /**
+     * Allows execution of code which may mutate a cache entry with exclusive
+     * access (including reads) to that entry.
+     * <p/>
+     * This enables a way to perform compound operations without transactions
+     * involving a cache entry atomically. Such operations may include mutations.
+     * <p/>
+     * The mutations can be potentially done in place avoiding expensive network transfers.
+     * An example is a value which is a list and you simply want to append a new element to it.
+     * <p/>
+     * An entry processor cannot invoke any cache operations, including processor operations.
+     * <p/>
+     * If executed in a JVM remote from the one invoke was called in, an AtomicOperation equal
+     * to the local one will execute the invocation. For remote to execution to succeed, the
+     * AtomicOperation implementation class must be in the excecuting class loader as must K and
+     * V if {@link Cache.MutableEntry#getKey()} or {@link Cache.MutableEntry#getValue()}
+     * is invoked.
+     *
+     *
+     * @param <K> the type of keys maintained by this cache
+     * @param <V> the type of cached values
+     * @author Greg Luck
+     * @author Yannis Cosmadopoulos
+     */
+    public interface AtomicOperation<K, V> {
+
+        //todo add processAll
+
+        /**
+         * Process an entry
+         * @param entry the entry
+         * @return the result
+         */
+        Object process(Cache.MutableEntry<K, V> entry);
     }
 }
