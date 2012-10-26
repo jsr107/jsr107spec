a0f8fd085c5bc1f9c8b1aac6330727d8a890a94f
gregrluck <gluck@gregluck.com>
2012-10-26 15:33:08 +1000

    Clarify JavaDoc

--- src/main/java/javax/cache/CacheWriter.java
+++ src/main/java/javax/cache/CacheWriter.java
@@ -11,7 +11,15 @@ package javax.cache;
 import java.util.Collection;
 
 /**
- * A CacheWriter is used for write-through and write-behind caching to a underlying resource.
+ * A CacheWriter is used for write-through to an underlying resource.
+ * <p/>
+ * The semantics of write-through when an <code>Exception</code> is thrown are implementation specific.
+ * <p/>
+ * The transactional semantics of write-through on a transactional cache are implementation specific.
+ * <p/>
+ * The entry passed into {@link #write(javax.cache.Cache.Entry)} is independent of the cache mapping for that key
+ * meaning that if the value changes in the cache or is removed it does not change the said entry.
+ *
  *
  * @param <K> the type of keys maintained by this map
  * @param <V> the type of mapped values
@@ -55,6 +63,8 @@ public interface CacheWriter<K, V> {
      * (by throwing an exception) after a partial success, the convention is that keys which have been erased successfully
      * are to be removed from the specified keys, indicating that the erase operation for the keys left in the collection
      * has failed or has not been attempted.
+     * <p/>
+     * Expiry of a cache entry is not a delete hence will not cause this method to be invoked.
      *
      * @param entries the entries that have been removed from the cache
      * @throws CacheException if ????? TODO describe when
