a2cf2576cb7ce91afce03e8c77e3afb4e795073a
gregrluck <gluck@gregluck.com>
2013-02-19 16:06:42 +1000

    Changed the RI and TCK to Java 7. The API remains at Java 6 as agreed with the Expert Group.

--- src/main/java/javax/cache/annotation/package-info.java
+++ src/main/java/javax/cache/annotation/package-info.java
@@ -6,9 +6,22 @@
  */
 
 /**
- * This package contains annotations for adding caching interceptors to java objects
- * 
+ * The annotations in this package provide method interceptors for user supplied classes.
+ *
+ * In the case of a the {@link CacheResult} annotation, if the cache can satisfy the request a result is returned
+ * by the method from cache, not from method execution. For the mutative annotations such as {@link CachePut}
+ * the annotation allows the cached value to be mutated so that it will be correct the next time {@link CacheResult} is used.
+ * <p/>
+ * Any operations against a cache via an annotation will have the same behaviour as if the {@link Cache} methods were used. So
+ * if the same underlying cache is used for an annotation and a direct API call, the same data would be returned. Annotations
+ * therefore provide an additional API for interacting with caches.
+ * <p/>
+ * In order to use these annotations, you'll need a library or framework which processes these annotations and intercepts calls
+ * to your application objects to provide the caching behaviour. This would commonly be provided by a dependency injection framework
+ * such as defined by CDI in Java EE.
+ *
  *  @author Eric Dalquist
+ *  @author Greg Luck
  *  @since 1.0
  */
 package javax.cache.annotation;
