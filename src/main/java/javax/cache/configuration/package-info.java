9e0d71dca1b0e5c87ce0334aa1b0100067e0d3d9
gregrluck <gluck@gregluck.com>
2013-05-31 11:20:45 -0700

    Finish JavaDoc and example

--- src/main/java/javax/cache/configuration/package-info.java
+++ src/main/java/javax/cache/configuration/package-info.java
@@ -15,12 +15,16 @@
  <code>
    MutableConfiguration config = new MutableConfiguration<String, Integer>()
           .setTypes(String.class, Integer.class);
+
    config.setStoreByValue(false).setStatisticsEnabled(true)
           .setExpiryPolicyFactory(
           FactoryBuilder.factoryOf(new Accessed<String, Integer>
           (new Duration(TimeUnit.HOURS, 1))));
  </code>
-
+ <p/>
+{@link OptionalFeature}, though not cache configuration,
+ makes known to user code those optional features supported by the caching
+ configuration to support runtime adaptation.
 
  @author Greg Luck
  @since 1.0
